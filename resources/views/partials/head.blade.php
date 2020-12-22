eb32becf50393423e185ccee6a72071c61ad66a5
niel63 <placmedia@gmail.com>
2020-12-22 01:12:36 -0500

    Firs commit

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -9,37 +9,23 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="title" content="Auto & Cartalk Free VIN checker">
 
-<!-- Canonical SEO -->
-<link rel="canonical" href="https://www.creative-tim.com/product/impact-design-system" />
 
 <!--  Social tags      -->
 <meta name="keywords" content=" ">
 <meta name="description" content=" ">
 
 <!-- Schema.org markup for Google+ -->
-<meta itemprop="name" content="Impact Design System by Creative Tim">
-<meta itemprop="description" content="Kick-Start Your Development With An Awesome Design System carefully designed for your online business showcase. It comes as a complete solution, with front pages and dashboard pages included.">
-
-<meta itemprop="image" content="https://s3.amazonaws.com/creativetim_bucket/products/296/original/opt_impact_thumbnail.jpg">
+<meta itemprop="name" content="Auto & Cartalk Free VIN checker">
 
 <!-- Twitter Card data -->
 <meta name="twitter:card" content="product">
-<meta name="twitter:site" content="@creativetim">
-<meta name="twitter:title" content="Impact Design System by Creative Tim">
-
-<meta name="twitter:description" content="Kick-Start Your Development With An Awesome Design System carefully designed for your online business showcase. It comes as a complete solution, with front pages and dashboard pages included.">
-<meta name="twitter:creator" content="@creativetim">
-<meta name="twitter:image" content="https://s3.amazonaws.com/creativetim_bucket/products/296/original/opt_impact_thumbnail.jpg">
+<meta name="twitter:title" content="Auto & Cartalk Free VIN checker">
 
 
 <!-- Open Graph data -->
 <meta property="fb:app_id" content="655968634437471">
-<meta property="og:title" content="Impact Design System by Creative Tim" />
+<meta property="og:title" content="Auto & Cartalk Free VIN checker" />
 <meta property="og:type" content="article" />
-<meta property="og:url" content="https://demos.creative-tim.com/impact-design-system/" />
-<meta property="og:image" content="https://s3.amazonaws.com/creativetim_bucket/products/296/original/opt_impact_thumbnail.jpg"/>
-<meta property="og:description" content="Kick-Start Your Development With An Awesome Design System carefully designed for your online business showcase. It comes as a complete solution, with front pages and dashboard pages included." />
-<meta property="og:site_name" content="Creative Tim" />
 
 <!-- Favicon -->
 <link rel="icon" href="{{asset('assets/img/favicon/favicon.png')}}" type="image/png">
